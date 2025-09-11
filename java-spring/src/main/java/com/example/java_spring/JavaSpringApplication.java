9a320b0720d1a169511031302183be51b502db9b
Guilherme <guifefo@gmail.com>
2025-09-10 21:35:55 -0300

    Add files via upload

---
+++ java-spring/src/main/java/com/example/java_spring/JavaSpringApplication.java
@@ -0,0 +1,17 @@
+package com.example.java_spring;
+
+import com.example.java_spring.controller.Welcome;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class JavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringApplication.class, args);
+
+	}
+
+}
