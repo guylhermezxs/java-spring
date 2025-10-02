49e0804da2a5d0a016fa122753f314a4e0a868e1
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-02 18:57:37 -0300

    .

---
+++ javaspring-aula6/src/main/java/com/example/javaspring_aula6/JavaspringAula6Application.java
@@ -0,0 +1,13 @@
+package com.example.javaspring_aula6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringAula6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringAula6Application.class, args);
+	}
+
+}
---
+++ javaspring-aula6/src/test/java/com/example/javaspring_aula6/JavaspringAula6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspring_aula6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringAula6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
