6f5527b0b026b0d625ba0f9d016adcd28c53ee62
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-08 20:25:49 -0300

    .

--- javaspring-aula7/src/main/java/com/example/javaspring_aula7/repository/FuncionarioRepository.java
+++ javaspring-aula7/src/main/java/com/example/javaspring_aula7/repository/FuncionarioRepository.java
@@ -4,6 +4,7 @@ import com.example.javaspring_aula7.model.FuncionarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +12,5 @@ import java.util.UUID;
 @Repository
 public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, UUID> {
     Optional<FuncionarioModel> findByEmail(String email);
+    List<FuncionarioModel> findByNome(String nome);
 }
--- javaspring-aula7/src/main/java/com/example/javaspring_aula7/service/FuncionarioService.java
+++ javaspring-aula7/src/main/java/com/example/javaspring_aula7/service/FuncionarioService.java
@@ -16,13 +16,35 @@ public class FuncionarioService {
         this.funcionarioRepository = funcionarioRepository;
     }
 
-    public List<FuncionarioModel> listarFuncionarios(){
+    public List<FuncionarioModel> listarFuncionario(){
         return funcionarioRepository.findAll();
     }
 
-    public FuncionarioModel salvar(FuncionarioModel funcionarioModel , UUID id){
-        if(funcionarioRepository.existsById(id)){
-
+    public FuncionarioModel salvarFuncionario(FuncionarioModel funcionarioModel){
+        if(funcionarioRepository.findByEmail(funcionarioModel.getEmail()).isPresent()){
+            throw new IllegalArgumentException("Funcionário já cadastrado!");
         }
+        return funcionarioRepository.save(funcionarioModel);
+    }
+
+    public void deletarFuncionario(UUID id){
+        if (funcionarioRepository.existsById(id)){
+           funcionarioRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Funcionário não encontrado!");
+        }
+    }
+
+    public FuncionarioModel atualizarFuncionario(UUID id , FuncionarioModel funcionario){
+        if(funcionarioRepository.existsById(id)){
+            funcionario.setId(id);
+           return funcionarioRepository.save(funcionario);
+        } else {
+            throw new RuntimeException("Funcionário não encontrado!");
+        }
+    }
+
+    public List<FuncionarioModel> buscarFuncionario(String nome){
+        return funcionarioRepository.findByNome(nome);
     }
 }
