e8c019d1b97138ecff7bcb775a0851514c86b999
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-16 18:54:51 -0300

    .

---
+++ javaspring-aula9/src/test/java/com/example/javaspring_aula9/JavaspringAula9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspring_aula9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringAula9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
