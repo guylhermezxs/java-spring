0efebbf0f242a22ce2b1f22469407cdea0961d00
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-17 19:20:24 -0300

    .

---
+++ javaspring-aula10/src/main/java/com/example/javaspring_aula10/JavaspringAula10Application.java
@@ -0,0 +1,13 @@
+package com.example.javaspring_aula10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringAula10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringAula10Application.class, args);
+	}
+
+}
---
+++ javaspring-aula10/src/test/java/com/example/javaspring_aula10/JavaspringAula10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspring_aula10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringAula10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
